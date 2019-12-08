7c1a27534b229f5dcba18fc37aea22815dbfb7b5
pllapallpal <pllapallpal@gmail.com>
2019-12-08 19:17:58 +0900

    Add readFile() method

--- src/pllapallpal/Logic.java
+++ src/pllapallpal/Logic.java
@@ -1,4 +1,28 @@
 package pllapallpal;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Logic {
+
+    Scanner fileInput;
+    private String data;
+    private final String PATH = "res/data.txt";
+
+    public Logic() {
+        fileInput = null;
+    }
+
+    public void readFile() {
+        try {
+            fileInput = new Scanner(new File(PATH));
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getData() {
+        return data;
+    }
 }
